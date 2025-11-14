70502592a57a8be34d6cbaf2e796c7893faa9781
Genaro Rojas <cgrojasr@live.com>
2025-11-13 20:43:58 -0500

    2025-2B Semana 2.1: Carrito del header muestra los productos que vamos agregando.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from './shared/footer/footer.component';
+import { ModalCarritoComponent } from "./shared/modal-carrito/modal-carrito.component";
+import { ModalLoginComponent } from "./shared/modal-login/modal-login.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet, 
-    HeaderComponent, 
-    FooterComponent, 
-    RouterModule
-  ],
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    RouterModule,
+    ModalCarritoComponent,
+    ModalLoginComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/shared/modal-carrito/modal-carrito.component.ts
+++ src/app/shared/modal-carrito/modal-carrito.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { ProductoCarritoModel } from '../../models/producto-model';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { CarritoStateService } from '../../services/carrito-state/carrito-state.service';
 
 @Component({
   selector: 'app-modal-carrito',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './modal-carrito.component.html',
   styleUrl: './modal-carrito.component.css'
 })
-export class ModalCarritoComponent {
+export class ModalCarritoComponent implements OnInit, AfterViewInit {
+  isOpen = true;
+  carrito: ProductoCarritoModel[] = [];
+  private modalElement!: HTMLElement | null;
+  private subs = new Subscription();
+  private showHandler?: () => void;
 
+  constructor(
+    private cookieService: CookieService,
+    private elementRef: ElementRef,
+    private carritoState: CarritoStateService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    // Cargar al iniciar
+    this.cargarCarrito();
+
+    // Refrescar cuando cambie el estado (por ejemplo, al agregar desde catálogo)
+    this.subs.add(
+      this.carritoState.cantidadCarrito$.subscribe(() => {
+        this.cargarCarrito();
+        this.cdr.detectChanges();
+      })
+    );
+  }
+
+  ngAfterViewInit(): void {
+    // Refrescar cada vez que el modal se vaya a mostrar
+    this.modalElement = this.elementRef.nativeElement.querySelector('#carritoModal');
+    if (this.modalElement) {
+      this.showHandler = () => {
+        this.cargarCarrito();
+        this.cdr.detectChanges();
+      };
+      this.modalElement.addEventListener('show.bs.modal', this.showHandler);
+    }
+  }
+
+  cargarCarrito(): void {
+    const carritoJson = this.cookieService.get('carrito');
+    this.carrito = carritoJson ? JSON.parse(carritoJson) : [];
+  }
+
+  close_OnClick(): void {
+    this.isOpen = false;
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+    if (this.modalElement && this.showHandler) {
+      this.modalElement.removeEventListener('show.bs.modal', this.showHandler);
+    }
+  }
 }
